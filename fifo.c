22c886638a0316f42d41c87472412017d365abe4
Maksim Perov <coder@frtk.ru>
2021-03-30 17:58:55 +0300

    FIFO type is added

--- fifo.c
+++ fifo.c
@@ -3,22 +3,30 @@
 #define FIFO_SIZE 10
 #define TYPE int
 
-TYPE fifo_array[FIFO_SIZE] = {0x0};
-unsigned int fifo_start = 0x0;
-unsigned int fifo_end = 0x0;
+typedef struct {
+    TYPE array[FIFO_SIZE];
+    unsigned int start;
+    unsigned int end;
+} FIFO;
 
-void fifo_print(TYPE *fifo) {
-    if ( (fifo_start != fifo_end) && (fifo_start < FIFO_SIZE) && (fifo_end < FIFO_SIZE) ) {
+void fifo_print(FIFO *fifo) {
+    if ( (fifo->start != fifo->end) && (fifo->start < FIFO_SIZE) && (fifo->end < FIFO_SIZE) ) {
         printf("all elements of FIFO: ");
         int i;
-        for (i = fifo_start; i < fifo_end; i++)
-            printf("%i", *(fifo + i));
+        for (i = fifo->start; i < fifo->end; i++)
+            printf("%i", fifo->array[i]);
         printf("\n");
     } else
         printf("FIFO is empty!\n");
 }
 
+void fifo_init(FIFO *fifo) {
+   fifo->start = fifo->end = 0;
+}
+
 void main() {
-    fifo_print(fifo_array);
+    FIFO fifo;
+    fifo_init(&fifo);
+    fifo_print(&fifo);
     return;
 }
