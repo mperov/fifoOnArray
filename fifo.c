fd86e80071dfc9b45929760b091d67510122fe0d
Maksim Perov <coder@frtk.ru>
2021-03-30 18:04:42 +0300

    func for pushing to FIFO is added

--- fifo.c
+++ fifo.c
@@ -10,18 +10,26 @@ typedef struct {
 } FIFO;
 
 void fifo_print(FIFO *fifo) {
-    if ( (fifo->start != fifo->end) && (fifo->start < FIFO_SIZE) && (fifo->end < FIFO_SIZE) ) {
+    if (fifo->start != fifo->end) {
         printf("all elements of FIFO: ");
         int i;
         for (i = fifo->start; i < fifo->end; i++)
-            printf("%i", fifo->array[i]);
+            printf("%i ", fifo->array[i]);
         printf("\n");
     } else
         printf("FIFO is empty!\n");
 }
 
+void fifo_push(FIFO *fifo, TYPE element) {
+    fifo->array[fifo->end++] = element;
+}
+
 void fifo_init(FIFO *fifo) {
    fifo->start = fifo->end = 0;
+   int i;
+   for(i = 0; i < FIFO_SIZE; i++) {
+       fifo_push(fifo, i);
+   }
 }
 
 void main() {
