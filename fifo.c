657e884007bf99c1d2968d53f600865adf85dd0c
Maksim Perov <coder@frtk.ru>
2021-03-31 10:51:53 +0300

    second test of FIFO is added

--- fifo.c
+++ fifo.c
@@ -37,25 +37,45 @@ int fifo_pop(FIFO *fifo, TYPE *element) {
 }
 
 void fifo_init(FIFO *fifo) {
-   fifo->start = fifo->end = 0;
-   int i;
-   for(i = 0; i < FIFO_SIZE; i++) {
-       fifo_push(fifo, i);
+    fifo->start = fifo->end = 0;
+    int i;
+    int error;
+    for(i = 0; i < FIFO_SIZE; i++) {
+        error = fifo_push(fifo, i);
+        if (error)
+            printf("error, line is %i\n", __LINE__);
    }
 }
 
 void main() {
     FIFO fifo;
+    int i;
+    int error;
     printf("---------------- First test ----------------\n");
     fifo_init(&fifo);
     fifo_print(&fifo);
-    int i;
     for (i = 0; i < FIFO_SIZE; i++) {
         TYPE element;
-        fifo_pop(&fifo, &element);
-        printf("element from FIFO: %i\n", element);
+        error = fifo_pop(&fifo, &element);
+        if (!error)
+            printf("element from FIFO: %i\n", element);
+        else
+            printf("error, line is %i\n", __LINE__);
         fifo_print(&fifo);
     }
     printf("================ First test ================\n");
+    printf("---------------- Second test ----------------\n");
+    fifo_init(&fifo);
+    fifo_print(&fifo);
+    for (i = 0; i < FIFO_SIZE + 5; i++) {
+        TYPE element;
+        error = fifo_pop(&fifo, &element);
+        if (!error)
+            printf("element from FIFO: %i\n", element);
+        else
+            printf("error, line is %i\n", __LINE__);
+        fifo_print(&fifo);
+    }
+    printf("================ Second test ================\n");
     return;
 }
