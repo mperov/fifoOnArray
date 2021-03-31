15f22e8ef6f6f242a6a5c7cbe17de5373c14c0f5
Maksim Perov <coder@frtk.ru>
2021-03-31 11:02:00 +0300

    reset of FIFO is added in pop function

--- fifo.c
+++ fifo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//#define DEBUG
 #define FIFO_SIZE 10
 #define TYPE int
 
@@ -23,6 +24,9 @@ void fifo_print(FIFO *fifo) {
 int fifo_push(FIFO *fifo, TYPE element) {
     if (fifo->end < FIFO_SIZE) {
         fifo->array[fifo->end++] = element;
+#ifdef DEBUG
+        printf("push: end is %i\n", fifo->end);
+#endif // DEBUG
         return 0;
     } else
         return -1;
@@ -31,24 +35,29 @@ int fifo_push(FIFO *fifo, TYPE element) {
 int fifo_pop(FIFO *fifo, TYPE *element) {
     if (fifo->start != fifo->end) {
         *element = fifo->array[fifo->start++];
+#ifdef DEBUG
+        printf("pop: start is %i\n", fifo->start);
+#endif // DEBUG
+        if (fifo->start == fifo->end)
+            fifo->start = fifo->end = 0;
         return 0;
     } else
         return -1;
 }
 
 void fifo_init(FIFO *fifo) {
-    fifo->start = fifo->end = 0;
     int i;
     int error;
     for(i = 0; i < FIFO_SIZE; i++) {
         error = fifo_push(fifo, i);
         if (error)
-            printf("error, line is %i\n", __LINE__);
+            printf("push error, line is %i\n", __LINE__);
    }
 }
 
 void main() {
     FIFO fifo;
+    fifo.start = fifo.end = 0;
     int i;
     int error;
     printf("---------------- First test ----------------\n");
@@ -60,7 +69,7 @@ void main() {
         if (!error)
             printf("element from FIFO: %i\n", element);
         else
-            printf("error, line is %i\n", __LINE__);
+            printf("pop error, line is %i\n", __LINE__);
         fifo_print(&fifo);
     }
     printf("================ First test ================\n");
@@ -73,7 +82,7 @@ void main() {
         if (!error)
             printf("element from FIFO: %i\n", element);
         else
-            printf("error, line is %i\n", __LINE__);
+            printf("pop error, line is %i\n", __LINE__);
         fifo_print(&fifo);
     }
     printf("================ Second test ================\n");
